763ef2a551eb5aeeddf6b1815eacfc90baf558c8
GitHub Action <action@github.com>
2023-02-13 11:42:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/vovabezhenar/foldersexclude/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/vovabezhenar/foldersexclude/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.vovabezhenar.foldersexclude.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vovabezhenar.foldersexclude.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/vovabezhenar/foldersexclude/services/MyProjectService.kt
+++ src/main/kotlin/com/github/vovabezhenar/foldersexclude/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.vovabezhenar.foldersexclude.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.vovabezhenar.foldersexclude.MyBundle
 
 class MyProjectService(project: Project) {
 
